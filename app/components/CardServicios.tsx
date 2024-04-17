c768f84ed20e904edfa4763ff28d0074948169a9
unknown <gerson_mtzvilla@hotmail.com>
2024-04-17 17:18:21 -0600

    V1.12

--- app/components/CardServicios.tsx
+++ app/components/CardServicios.tsx
@@ -1,29 +1,22 @@
-import Btn from "./Btn";
-
+import React from 'react';
+import Btn  from "./Btn";
 import espadas from "../src/espadas-tres.webp"
 import parrilla from "~/src/parrillada1.webp"
 import tacos from "../src/tacos.webp"
 import mobiliario from "../src/moviliario.webp"
 import meseros from "../src/meseros.webp"
 import dj from "../src/DJ.webp"
-
-import logoEspadas from "../src/logosSvg/espadas-icon.svg"
-import logoParrilla from "../src/logosSvg/parrilla-icon.svg"
-import logoTaco from "../src/logosSvg/taco-icon.svg"
-import logoCarpa from "../src/logosSvg/mobiliario-icon.svg"
-import logoMesero from "../src/logosSvg/mesero-icon.svg"
-import logoDj from "../src/logosSvg/dj-icon.svg"
+import { DjIcon, EspadasIcon, MeserosIcon, MobiliarioIcon, ParrilladaIcon, TacosIcon } from './IconsSvg';
 
 const CardServicios = () => {
-
   const cardsServicios = [
-    { title: "Espadas Brasileñas", route:"espadas-brasilenas", alt: "Espadas brasileñas", src: espadas, logo: logoEspadas },
-    { title: "Parrilladas", route:"parrilladas", alt: "Parrilladas", src: parrilla, logo: logoParrilla },
-    { title: "Buffet de Tacos", route:"tacos", alt: "buffet de tacos", src: tacos, logo: logoTaco },
-    { title: "Mobiliario", route:"mobiliario", alt: "mobiliario", src: mobiliario, logo: logoCarpa },
-    { title: "Meseros", route:"personal", alt: "meseros", src: meseros, logo: logoMesero },
-    { title: "Audio Dj", route:"personal", alt: "Dj", src: dj, logo: logoDj }
-  ]
+    { title: "Espadas Brasileñas", route:"espadas-brasilenas", alt: "Espadas brasileñas", src: espadas, logo: <EspadasIcon /> },
+    { title: "Parrilladas", route:"parrilladas", alt: "Parrilladas", src: parrilla, logo: <ParrilladaIcon /> },
+    { title: "Buffet de Tacos", route:"tacos", alt: "buffet de tacos", src: tacos, logo: <TacosIcon /> },
+    { title: "Mobiliario", route:"mobiliario", alt: "mobiliario", src: mobiliario, logo: <MobiliarioIcon /> },
+    { title: "Meseros", route:"personal", alt: "meseros", src: meseros, logo: <MeserosIcon /> },
+    { title: "Audio Dj", route:"personal", alt: "Dj", src: dj, logo: <DjIcon /> }
+  ];
 
   return (
     cardsServicios.map((card,index) => (
@@ -32,7 +25,7 @@ const CardServicios = () => {
           <img className="object-cover h-full w-full" src={card.src} alt={`Imagen donde se ve el servicio de ${card.alt} de banquetes Jorgio`} />
         </div>
         <div className="p-6 flex flex-col gap-4 justify-between">
-          <div className="bg-cover bg-no-repeat w-8 h-8" style={{ backgroundImage: `url(${card.logo})` }}></div>
+          <div className="bg-cover bg-no-repeat w-8 h-8">{card.logo}</div>
           <h5 className="mb-2 block font-variable text-2xl text-heading font-bold">
             {card.title}
           </h5>
@@ -44,4 +37,3 @@ const CardServicios = () => {
 };
 
 export default CardServicios;
-
