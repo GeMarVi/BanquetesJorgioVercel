4da3ced8472b7731f99d9f1d5f592e88405c0a6a
gerson martinez villanueva <gerson_mtzvilla@hotmail.com>
2024-08-28 22:12:31 -0600

    delete tag google ads

--- app/root.tsx
+++ app/root.tsx
@@ -60,15 +60,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
                <Scripts />
                <Footer />
             </ScrollProvider>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16671910917">
-            </script>
-            <script>
-               window.dataLayer = window.dataLayer || [];
-               function gtag(){dataLayer.push(arguments)}
-               gtag('js', new Date());
-
-               gtag('config', 'AW-16671910917');
-            </script>
          </body>
       </html>
    );
